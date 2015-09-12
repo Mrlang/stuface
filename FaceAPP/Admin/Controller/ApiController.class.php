de50cbb38f6f289349b827cc7de5a8ba2aaff571
duwonders <752046798@qq.com>
2015-09-12 21:21:42 +0800

    dsa

--- FaceAPP/Admin/Controller/ApiController.class.php
+++ FaceAPP/Admin/Controller/ApiController.class.php
@@ -3,7 +3,10 @@ namespace Admin\Controller;
 use Think\Controller;
 class ApiController extends Controller {
 	public function index(){
-		$message = M('image')->join('tbl_user ON tbl_image.uid = tbl_user.uid')->order('vote')->select();
+		$where = [
+			'is_pass' => 1,
+		];
+		$message = M('image')->where($where)->join('tbl_user ON tbl_image.uid = tbl_user.uid')->order('vote')->select();
 		$this->assign('message', $message);
 		$this->display();
 	}
